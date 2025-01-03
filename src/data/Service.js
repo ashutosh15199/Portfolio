34206c59667b7a7aec2f476005cf847719d35168
Ashutosh_tiwari6554843 <ashutoshkumartiwari686@gail.com>
2025-01-03 17:56:59 +0530

    commit in the ashutosh

---
+++ src/data/Service.js
@@ -0,0 +1,70 @@
+import javalogoimg from '../assets/image/java-logo-1.png'
+import javaScript from '../assets/image/javascript.svg'
+import html from '../assets/image/html.svg'
+import css from '../assets/image/css.svg'
+import reactjs from '../assets/image//react.svg'
+import mongo from '../assets/image//mongodb.svg'
+import mysql from '../assets/image//mysql.svg'
+import nodejs from '../assets/image//nodejs.svg'
+import springboot from '../assets/image//spring-boot-logo.png'
+import postgresql from '../assets/image/postgresql.svg'
+import logic from '../assets/image/logical-thinking.webp'
+import firebase from '../assets/image/firebase.svg'
+import git from '../assets/image/git.svg'
+import github from '../assets/image/images.png'
+
+const serviceData=[
+   {
+    title:"Front_End Development",
+    image:[
+        {
+            javaScript,
+            reactjs,
+            html,
+            css
+        },
+      
+    ],
+    description:"I create captivating mobile apps from concept to deployment for iOS and Android. Using cutting-edge technologies, I ensure seamless performance, intuitive interfaces, and robust functionality that align with your business goals. Enjoy a flawless user experience and outstanding results.",
+   },
+   {
+    title:"Back_End Development",
+    image:[
+        {
+            javalogoimg,
+            nodejs,
+            springboot
+            
+        },
+      
+    ],
+    description: "I enhance digital applications with robust, scalable backend infrastructures. I develop efficient database structures, APIs, and configure servers for optimal performance, security, and scalability, ensuring your applications handle high traffic and complex data management seamlessly. Rely on strong backend solutions.",
+   },
+   {
+    title:"Product Strategy",
+    image:[
+        {
+            git,
+            github,
+            logic,
+            firebase
+        },
+      
+    ],
+    description:"I collaborate to define clear goals, target audiences, and a success roadmap. My expertise in product ideation and market analysis ensures your product meets user needs and aligns with your business strategy for long-term growth and full potential realization. Drive your product's success with strategic planning.",
+   },
+   {
+    title:"Database Management",
+    image:[
+        {
+            mongo,
+            postgresql,
+            mysql,
+            
+        },
+      
+    ],
+    description:"I manage and optimize your database systems for performance, reliability, and scalability. With expertise in SQL and NoSQL databases, I design schemas, write complex queries, and implement best practices for data integrity and security. Ensure your data is managed effectively and efficiently.",
+  
+   }
+]
